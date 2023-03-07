7f21018c4db4bc2d447e8db421681f54b4b9ccdd
mhenr <112859034+mhenr@users.noreply.github.com>
2023-03-07 09:18:35 +0100

    change title in gui

--- Server/GuiMapView.py
+++ Server/GuiMapView.py
@@ -4,10 +4,11 @@ import tkintermapview
 # create tkinter window
 root_tk = tkinter.Tk()
 root_tk.geometry(f"{800}x{600}")
-root_tk.title("map_view_example.py")
+root_tk.title("p2.py")
 
 # create map widget
-map_widget = tkintermapview.TkinterMapView(root_tk, width=800, height=600, corner_radius=0)
+map_widget = tkintermapview.TkinterMapView(
+    root_tk, width=800, height=600, corner_radius=0)
 map_widget.place(relx=0.5, rely=0.5, anchor=tkinter.CENTER)
 
 
