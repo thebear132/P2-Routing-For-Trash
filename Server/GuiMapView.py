18c1d9ec9b1829f2bc96844b47ea288fbc2f49c6
mhenr <112859034+mhenr@users.noreply.github.com>
2023-03-07 11:47:24 +0100

    added en fil af gui til Tkinter, lavet makers og connetede dem

--- Server/GuiMapView.py
+++ Server/GuiMapView.py
@@ -1,15 +1,54 @@
-import tkinter
+import tkinter as tk
 import tkintermapview
+import customtkinter
+import gmaps
 
 # create tkinter window
-root_tk = tkinter.Tk()
-root_tk.geometry(f"{800}x{600}")
-root_tk.title("p2.py")
+window = tk.Tk()
+window.geometry(f"{800}x{600}")
+window.title("p2.py")
 
 # create map widget
 map_widget = tkintermapview.TkinterMapView(
-    root_tk, width=800, height=600, corner_radius=0)
-map_widget.place(relx=0.5, rely=0.5, anchor=tkinter.CENTER)
+    window, width=800, height=600, corner_radius=0)
+map_widget.place(relx=0.5, rely=0.5, anchor=tk.CENTER)
+# creating starting position in Aalborg
+map_widget.set_position(57.05, 9.925)
+map_widget.set_zoom(13)
+
+# Mouse events on the map
 
 
-root_tk.mainloop()
+def add_marker_event(coords):
+    print("Add marker:", coords)
+    new_marker = map_widget.set_marker(coords[0], coords[1], text="new marker")
+
+
+map_widget.add_right_click_menu_command(label="Add Marker",
+                                        command=add_marker_event,
+                                        pass_coords=True)
+
+# set a position marker
+marker_2 = map_widget.set_marker(57.0509139, 9.9213629)
+marker_3 = map_widget.set_marker(57.0574062, 9.9224943)
+
+# # methods
+# marker_3.set_position(...)
+# marker_3.set_text(...)
+# marker_3.change_icon(new_icon)
+# marker_3.hide_image(True)  # or False
+# marker_3.delete()
+
+# set a path
+path_1 = map_widget.set_path(
+    [marker_2.position, marker_3.position])
+
+
+# # methods
+# path_1.set_position_list(new_position_list)
+# path_1.add_position(position)
+# path_1.remove_position(position)
+# path_1.delete()
+
+
+window.mainloop()
--- Server/gui.py
+++ Server/gui.py
@@ -1,14 +1,15 @@
 # https://realpython.com/pysimplegui-python/#getting-started-with-pysimplegui
 
 import PySimpleGUI as sg
+import GuiMapView as gmv
 
 sg.theme('DarkBlue3')
 
-map_column = sg.Column([
-    [sg.Canvas(key='-graph-', size=(600, 500))],
-    [sg.Text("VORES MAP SKAL VÆRE HER")]
-    ]
-)
+map_column = sg.Column(
+    [[sg.Canvas(key='-graph-', size=(600, 500))], [sg.Text("VORES MAP SKAL VÆRE HER")]])
+
+# [sg.Graph(
+#     gmv, (125, 50), (0, 0), (125, 50), key='graph')]],
 
 layout = [
     [map_column, sg.VSeperator(), sg.Text('Some text on Row 1')]
@@ -16,15 +17,14 @@ layout = [
 
 # Create the Window
 window = sg.Window('Window Title', layout, finalize=True)
-#window.Maximize()
+# window.Maximize()
 window.size = (1200, 600)
 
 
 while True:
     event, values = window.read()
-    if event == sg.WIN_CLOSED or event == 'Cancel': # if user closes window or clicks cancel
+    if event == sg.WIN_CLOSED or event == 'Cancel':  # if user closes window or clicks cancel
         break
     print('You entered ', values[0])
 
 window.close()
-
