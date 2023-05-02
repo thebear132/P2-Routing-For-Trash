43c8f67e02646f2b28b402f82af6c15be076d50c
Andersen2407 <nicolai2407@gmail.com>
2023-05-02 12:32:18 +0200

    GUI progress
    Testing with datastorage.py to handle real data.

--- GUI/plotly gui.py
+++ GUI/plotly gui.py
@@ -2,26 +2,44 @@ import plotly.express as px
 import plotly.graph_objects as go
 import sqlite3
 import pandas as pd
-
+import json
 
 
 from DELETEME.datastorage import DataStorage
 myDS = DataStorage(69, 1000, ADDR=('127.0.0.1', 12445))
-print(myDS.fetch_latest_rows(myDS.table_name, "main"))
+DS_last_rows = myDS.fetch_latest_rows(myDS.table_name, "main")
+
+molok_id=[]; lon=[]; lat=[]; fill_pct=[]; timestamp=[]
+for i in DS_last_rows:
+    molok_id.append(i[1])
+    fill_pct.append(i[3])
+    timestamp.append(i[4])
+
+    coords = json.loads(i[2].replace(' ',',', 1))
+    lon.append(coords[0])
+    lat.append(coords[1])
+
+    #print(molok_id, lon, lat, fill_pct, timestamp)
 
 
-print("Getting data...")
-df = px.data.carshare()
-print(df.head(10))
-#print(df.tail(10))
+df = pd.DataFrame(DS_last_rows)
+df.columns = ["maxID", "MolokID", "Coords", "Fill_pct", "Timestamp"]
 
-df_10 = df.head(10)
 
-fig = px.scatter_mapbox(df, 
-                        lon=df['centroid_lon'], 
-                        lat=df['centroid_lat'], 
+#print("Test shit")
+y = df['Coords'].str.replace(' ', ',', 1).str.strip('[ ]')
+df[['lat', 'lon']] = y.str.split(',', expand=True)
+df = df.drop(columns=["Coords"])
+print(df)
+
+exit(0)
+
+fig = px.scatter_mapbox(myDS,
+                        lon=df[lon], 
+                        lat=lat,
+                        hover_data = molok_id,
                         zoom=10, #Starts zoom
-                        color= df['peak_hour'],
+                        color=fill_pct,
                         color_continuous_scale=px.colors.diverging.RdYlGn_r,
                         width=1200, height=900, 
                         title='Car share scatter map',
@@ -37,8 +55,8 @@ fig.update_layout(
 fig.add_trace(go.Scattermapbox(
     mode = "lines",
     hoverinfo= "skip",
-    lon = df_10['centroid_lon'],
-    lat = df_10['centroid_lat'],
+    lon = lon,
+    lat = lat,
     line = dict(width = 1, color = 'blue'),
     opacity = 0.5))
 
