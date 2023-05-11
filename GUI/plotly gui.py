2cb83a82bc671816f3517972e6ddbb9a39bc47a3
TheBear <drachmann.bjorn@gmail.com>
2023-05-11 11:08:59 +0200

    Updated factorizing

--- GUI/plotly gui.py
+++ GUI/plotly gui.py
@@ -26,8 +26,9 @@ from DELETEME.datastorage import DataStorage
 # Start en simulation (start Simulation.py og dataStorage.py og få data)
 # Plot molokkerne igen med deres nye fill_pct's (de burde være gået op nu)
 
-app = Dash(__name__)
 
+
+app = Dash(__name__)
 
 
 # Layout consist of every figure, graph and models on the GUI
@@ -47,34 +48,45 @@ app.layout = html.Div([
                 dcc.Slider(0, 10, 1,
                     value=5,
                     id='nrMoloksSlider'),
-                
+                #Radio items
+                dcc.RadioItems(["Simulation", "SigFox"], "Simulation", id="radioItems", inline=True, style={"margin-right": "30px","padding": "5px"}),
+
                 #Input (seed)
                 html.Label(html.B("Seed: ")),
                 dcc.Input(id="seedText", type="text", placeholder="Seed", style={"width": "93px"}),
                 
-                #Dogshit button
+                #Create table nutton
                 html.Button("Create table", id='CreateTableButton', style={"width": "100px"})
 
             ], style={'display' :'flex', 'flex-direction': 'column'}),
 
             dcc.Markdown("___", style={"margin-top": "20px"}), # HORIZONTAL LINE DIVIDER            
             
-            html.H2("Mode"),
+            html.H2("Show table"),
             html.Div(children=[
-                #Radio items
-                dcc.RadioItems(["Simulation", "SigFox"], "Simulation", id="radioItems", inline=True, style={"margin-right": "30px","padding": "5px"}),
-
-                #IP-address
-                dcc.Input(type="text", id="IPTextInput", value="127.0.0.1", style={"width": "70px", "margin-right": "30px"}),
-
                 #Dropdown
-                dcc.Dropdown(id="tableDropdown", searchable=True, placeholder="Select table", style={"width": "200px"})
+                dcc.Dropdown(id="tableDropdown", searchable=True, placeholder="Select table", style={"width": "200px"}),
             
             ], style={"display": "flex", "flex-direction": "row", 'width': '40vw'}),
             
             dcc.Markdown("___", style={"margin-top": "20px"}),  # HORIZONTAL LINE DIVIDER
             html.Br(),
             html.Div(id='my-output'),
+
+            html.H2("Start simulation"),
+            html.Div(children=[
+                
+                #IP-address
+                dcc.Input(type="text", id="IPTextInput", value="127.0.0.1", style={"width": "70px", "margin-right": "30px"}),
+
+                #Start sim / get sigfox
+                html.Button("Gather data", id='gatherDataButton', style={"width": "100px"}),
+
+            
+            ], style={"display": "flex", "flex-direction": "row", 'width': '40vw'}),
+            
+            dcc.Markdown("___", style={"margin-top": "20px"}),  # HORIZONTAL LINE DIVIDER
+            html.Br(),
         ])
         
         ], style={'display' :'flex', 'flex-direction': 'row'})
@@ -85,15 +97,20 @@ app.layout = html.Div([
         Output('tableDropdown', "options"),
         Input('CreateTableButton', 'n_clicks'),
         State('nrMoloksSlider', 'value'),
-        State('seedText', 'value'))
-def callCreateTable(n_clicksButton, nrMoloksSlider, seedText):
-    temporaryDS = DataStorage(int(seedText), int(nrMoloksSlider))
-    if seedText != None:
-        print(f"Creating table with {nrMoloksSlider} moloks and seed {seedText}")
-    else: print("Seed was None, probably by Dash in the start")
-    tableNames = temporaryDS.get_tablenames()
-    del temporaryDS
-    return tableNames
+        State('seedText', 'value'),
+        State('radioItems', 'value'))
+def callCreateTable(n_clicksButton, nrMoloksSlider, seedText, typeTable):
+    #print("@ CALL_CREATE_TABLE() |", n_clicksButton, nrMoloksSlider, seedText, typeTable)
+    
+    tmpDS = DataStorage()
+    typeTable = typeTable.lower()
+    if n_clicksButton != None:
+        if 'sim' in typeTable: typeTable = 'sim'
+        tmpDS.create_table(seedText, nrMoloksSlider, typeTable.lower())
+    
+    return tmpDS.get_tablenames()
+
+
 
 
 #Disables the ip-input textbox if SigFox is selected
@@ -108,16 +125,23 @@ def update_dropdownIP_color(value):
 @app.callback(Output("Map", "figure"),
               Input('tableDropdown', "value"))
 def Callupdate_scatterMap(selectedTable):
-    print("Selected table:", selectedTable)
-    if selectedTable == None:
-        myDS = DataStorage(69, 1000)
-    else:
-        a = selectedTable[8:].split("_")
-        seed = int(a[0])
-        moloks = int(a[1][4:])        
-        myDS = DataStorage(seed, moloks)
-    latest_rows = myDS.fetch_latest_rows(myDS.table_name, "main")
-    del myDS
+    print("\n@ Callupdate_scatterMap()")
+    if selectedTable == None: return None
+    
+    a = selectedTable.split("_")
+    type = a[0]
+    seed = int(a[1][4:])
+    moloks = int(a[2][4:])
+    print("HEJ")
+    print(selectedTable, seed, moloks)
+    tmpDS = DataStorage()
+    a = tmpDS.select_table(selectedTable, seed, moloks)
+    
+
+    latest_rows = tmpDS.fetch_latest_rows("main")
+    if len(latest_rows) == 0:
+        print("[!] THIS TABLE IS EMPTY")
+        return None
     
     df = pd.DataFrame(latest_rows)
     df.columns = ["maxID", "MolokID", "Coords", "Fill_pct", "Timestamp"]        # Set names of columns
@@ -131,22 +155,28 @@ def Callupdate_scatterMap(selectedTable):
     df = df.astype('float')
     df["MolokID"] = df["MolokID"].astype('int')
 
-    fig = px.scatter_mapbox(df,
-                            title='Route planner',
-                            zoom=10, #Starts zoom
-                            range_color=[0,100],
-                            color_continuous_scale=px.colors.sequential.Blackbody_r,
-                            #width=1280, height=720,
-                            size=[1 for i in range(len(df["lon"]))],
-                            opacity=1,
-                            size_max=20,
-                            hover_name="Molok:" + df["MolokID"].astype(str),
-                            color="Fill_pct",
-                            lon="lon",
-                            lat="lat",)
+    #print(df)
+    # fig = px.scatter_mapbox(df,
+    #                         title='Route planner',
+    #                         zoom=10, #Starts zoom
+    #                         range_color=[0,100],
+    #                         color_continuous_scale=px.colors.sequential.Blackbody_r,
+    #                         #width=1280, height=720,
+    #                         size=[1 for i in range(len(df["lon"]))],
+    #                         opacity=1,
+    #                         size_max=20,
+    #                         hover_name="Molok:" + df["MolokID"].astype(str),
+    #                         color="Fill_pct",
+    #                         lon="lon",
+    #                         lat="lat",)
+
+    fig = px.scatter_mapbox(df, lon="lon", lat="lat", color="Fill_pct")
+
+    fig.show()
 
     #Setting for the maps 
     fig.update_layout(mapbox_style="open-street-map", margin ={'l':0,'t':0,'b':0,'r':0})
+    
     return fig
 
 
